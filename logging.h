70ee80c8037b641aefe85d110fcc0070fa6a7a71
Atomic Spacecat <kutinar@gmail.com>
2022-10-10 22:09:06 +0200

    Move to PlatformIO build system

--- logging.h
+++
@@ -1,5 +0,0 @@
-#define DEBUG(X) Serial.print("dbug:"); Serial.println(X)
-#define INFO(X) Serial.print("info:"); Serial.println(X)
-#define WARN(X) Serial.print("warn:"); Serial.println(X)
-
-#define DEBUG_HEXVALUE(X,Y) Serial.print("dbug:"); Serial.print(X); Serial.print(Y, HEX)
